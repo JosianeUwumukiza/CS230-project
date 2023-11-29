2f468595c821db8f207c4276ad4a1dca27ca3717
aubreycantrell <152217351+aubreycantrell@users.noreply.github.com>
2023-11-29 18:22:07 -0500

    Update HollywoodGraph.java

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -1,3 +1,9 @@
+//*********************************************************************************************************
+//
+//
+//use an undirected graph G with vertices representing movies and actors. Edges will reflect the relationship 
+//“actor played role in movie”. Every actor A that played in a movie M will result in an undirected edge
+//*********************************************************************************************************
 public class HollywoodGraph {
 
 
