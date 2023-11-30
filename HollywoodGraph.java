5e2cfc4f07df9ef488033c6b845e1cd13f10ca76
aubreycantrell <152217351+aubreycantrell@users.noreply.github.com>
2023-11-30 16:44:43 -0500

    Update HollywoodGraph.java

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -1,30 +1,36 @@
+import java.io.File;
+import java.util.Scanner;
+import javafoundations.*;
+import java.util.ArrayList;
+import java.io.IOException;
+
 //*********************************************************************************************************
 //
 //
 //use an undirected graph G with vertices representing movies and actors. Edges will reflect the relationship 
 //“actor played role in movie”. Every actor A that played in a movie M will result in an undirected edge
 //*********************************************************************************************************
+public class HollywoodGraph<T> { //implements Graph<T> {
 
-
-
-
-
-public class HollywoodGraph<T> implements Graph<T> {
-
-    private ArrayList<T> names;
-    private AdjListsGraph<]<T> adj;
+    private ArrayList<String> names;
+    private AdjListsGraph<String> adj;
     
     public HollywoodGraph(){
         names = new ArrayList<String>();
-        adj = new AdjListsGraph<]<T>();
+        adj = new AdjListsGraph<String>();
     }
     
     public void graphBuilder(String fName){
         try{
             Scanner scan = new Scanner (new File(fName)).useDelimiter(",");
+            
+            String titles = scan.nextLine();
+            
              while(scan.hasNextLine()){
                 String movie = scan.next();
+                System.out.println(movie);
                 String actor = scan.next();
+                System.out.println(actor);
                 if (names.contains(movie)){ //if movie is in vertex
                     // find index in arraylist
                     //add to linked list in array list
@@ -42,11 +48,24 @@ public class HollywoodGraph<T> implements Graph<T> {
                     adj.addVertex(actor);
                 }//if name is not in vertex
                 adj.addEdge(movie,actor);
+                //scan.nextLine();
             }
+            scan.close();
         }catch(IOException ex){
             System.out.println(ex);
         }
-        scan.close();
+        
+    }
+
+    public String toString(){
+        return adj.toString();
+    }
+    
+    public static void main(String[] args){
+        HollywoodGraph<String> hollywood = new HollywoodGraph<String>();
+        
+        hollywood.graphBuilder("small_castGender.txt");
+        System.out.println(hollywood.toString());
     }
 
   /** Returns true if this graph is empty, false otherwise. */
