2ec10446b3e6ea3a01e6ba5dfe9f4725eaaeee69
lburg113 <152217390+lburg113@users.noreply.github.com>
2023-11-30 20:15:45 -0500

    Update HollywoodGraph.java

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -70,50 +70,72 @@ public class HollywoodGraph<T> { //implements Graph<T> {
     }
 
   /** Returns true if this graph is empty, false otherwise. */
-   public boolean isEmpty();
+    public boolean isEmpty(){
+        return adj.isEmpty();
+    }
 
-   /** Returns the number of vertices in this graph. */
-   public int getNumVertices();
+    /** Returns the number of vertices in this graph. */
+    public int getNumVertices(){
+        return adj.getNumVertices();
+    }
 
-   /** Returns the number of arcs in this graph. */
-   public int getNumArcs();
+    /** Returns the number of arcs in this graph. */
+    public int getNumArcs(){
+        System.out.println("There are no arcs in this graph, sorry!");
+        return -1;
+    }
 
-   /** Returns true iff a directed edge exists b/w given vertices */
-   public boolean isArc (T vertex1, T vertex2);
+    /** Returns true iff a directed edge exists b/w given vertices */
+    public boolean isArc (T vertex1, T vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+        return false;
+    }
 
-   /** Returns true iff an edge exists between two given vertices
-   * which means that two corresponding arcs exist in the graph */
-   public boolean isEdge (T vertex1, T vertex2);
+    /** Returns true iff an edge exists between two given vertices
+     * which means that two corresponding arcs exist in the graph */
+    public boolean isEdge (T vertex1, T vertex2){
+        return adj.isEdge(vertex1, vertex2);
+    }
 
-   /** Adds a vertex to this graph, associating object with vertex.
-   * If the vertex already exists, nothing is inserted. */
-   public void addVertex (T vertex);
+    /** Adds a vertex to this graph, associating object with vertex.
+     * If the vertex already exists, nothing is inserted. */
+    public void addVertex (T vertex){
+        adj.addVertex(vertex);
+    }
 
-   /** Removes a single vertex with the given value from this graph.
-   * If the vertex does not exist, it does not change the graph. */
-   public void removeVertex (T vertex);
+    /** Removes a single vertex with the given value from this graph.
+     * If the vertex does not exist, it does not change the graph. */
+    public void removeVertex (T vertex){
+        adj.removeVertex(vertex);
+    }
 
-   /** Inserts an arc between two vertices of this graph,
-   * if the vertices exist. Else it does not change the graph. */
-   public void addArc (T vertex1, T vertex2);
+    /** Inserts an arc between two vertices of this graph,
+     * if the vertices exist. Else it does not change the graph. */
+    public void addArc (T vertex1, T vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+    }
 
-   /** Removes an arc between two vertices of this graph,
-   * if the vertices exist. Else it does not change the graph. */
-   public void removeArc (T vertex1, T vertex2);
+    /** Removes an arc between two vertices of this graph,
+     * if the vertices exist. Else it does not change the graph. */
+    public void removeArc (T vertex1, T vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+    }
 
-   /** Inserts an edge between two vertices of this graph,
-   * if the vertices exist. Else does not change the graph. */
-   public void addEdge (T vertex1, T vertex2);
+    /** Inserts an edge between two vertices of this graph,
+     * if the vertices exist. Else does not change the graph. */
+    public void addEdge (T vertex1, T vertex2){
+        adj.addEdge(vertex1, vertex2);
+    }
 
-   /** Removes an edge between two vertices of this graph,
-   if the vertices exist, else does not change the graph. */
-   public void removeEdge (T vertex1, T vertex2);
+    /** Removes an edge between two vertices of this graph,
+    if the vertices exist, else does not change the graph. */
+    public void removeEdge (T vertex1, T vertex2){
+        adj.removeEdge(vertex1, vertex2);
+    }
 
-   /** Returns a string representation of the graph. */
-   public String toString();
-
-   /** Saves the current graph into a .tgf file.
-   If it cannot write the file, a message is printed. */
-   public void saveTGF(String tgf_file_name);
-  
+    /** Saves the current graph into a .tgf file.
+    If it cannot write the file, a message is printed. */
+    public void saveTGF(String tgf_file_name){
+        adj.saveTGF(tgf_file_name);
+    }
 }
