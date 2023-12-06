416a4ed471f69dc9497d192aa0777e7585629875
aubreycantrell <152217351+aubreycantrell@users.noreply.github.com>
2023-12-05 19:41:38 -0500

    Update HollywoodGraph.java

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -5,12 +5,12 @@
  * @author Aubrey, Josiane, Lauren
  * @version 12/1/23
  */
-
 import java.io.File;
 import java.util.Scanner;
 import javafoundations.*;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 //*********************************************************************************************************
 //
@@ -18,24 +18,18 @@ import java.io.IOException;
 //use an undirected graph G with vertices representing movies and actors. Edges will reflect the relationship 
 //“actor played role in movie”. Every actor A that played in a movie M will result in an undirected edge
 //*********************************************************************************************************
-public class HollywoodGraph<T> {
+public class HollywoodGraph<T> { //implements Graph<T> {
 
     private ArrayList<String> names;
     private AdjListsGraph<String> adj;
+    private ArrayList<Movie> movies; //holds genders, create movie class
 
-    /**
-     * Constructor for class HollywoodGraph
-     */
     public HollywoodGraph(){
         names = new ArrayList<String>();
         adj = new AdjListsGraph<String>();
+        movies = new ArrayList<Movie>();
     }
-    
-    /**
-     * Creates a graph from the input file
-     * 
-     * @param  fName  the name of the file to turn into a graph
-     */
+
     public void graphBuilder(String fName){
         try{
             Scanner scan = new Scanner (new File(fName)).useDelimiter(",");
@@ -47,22 +41,42 @@ public class HollywoodGraph<T> {
                 //System.out.println(movie);
                 String actor = scan.next().replace("\"", "");
                 //System.out.println(actor);
-                if (names.contains(movie)){ //if movie is in vertex
+                int index;
+                if (!names.contains(movie)){ //if movie is in vertex
                     // find index in arraylist
                     //add to linked list in array list
+                    //adj.addVertex(movie);
+
+                    //using classes:
+                    names.add(movie);
                     adj.addVertex(movie);
-                }else // if movie not in vertex
-                {   names.add(movie);
-                    adj.addVertex(movie);
+                    //create new movie object
+                    Movie m = new Movie(movie);
+                    movies.add(m);
+                    //find way to create indiv titles for each movie
+                    //if necessary
                 }
                 //get next token for name
-                if(names.contains(actor)){//if name is in vertex
-                    adj.addVertex(actor);
-                }
-                else{
+                adj.addVertex(actor);
+                if(!names.contains(actor)){//if name is in vertex
                     names.add(actor);
-                    adj.addVertex(actor);
-                }//if name is not in vertex
+                }
+
+                //adding actor to movie arraylist object
+                //Movie m = new Movie(movie);
+                scan.next();
+                scan.next();
+                scan.next();
+                String gender = scan.next().replace("\"", "");
+                System.out.println("name"+ actor + " gender: " + gender);
+                Actor a = new Actor(actor,gender);
+                
+                for (int i = 0; i < movies.size(); i++){
+                    if (movies.get(i).getName().equals(movie)){
+                        movies.get(i).addActor(a);
+                    }
+                } 
+
                 adj.addEdge(movie,actor);
                 scan.nextLine();
             }
@@ -70,113 +84,108 @@ public class HollywoodGraph<T> {
         }catch(IOException ex){
             System.out.println(ex);
         }
-
+        System.out.println(m.toString());
     }
 
-    /** 
-     * Checks if the graph has any vertices
-     * 
-     * @return  true if this graph is empty, false otherwise.  
-     */
-    public boolean isEmpty(){
-        return adj.isEmpty();
+    public boolean genderAnalysis(String movieName){
+        //int index;
+        //index = movies.indexOf(movieName);
+        //return movies.get(index).ratio();
+        boolean answer = false;
+        for (int i = 0; i < movies.size(); i++){
+            System.out.println(movies.get(i).getName());
+            if (movies.get(i).getName().equals(movieName)){
+                System.out.println("selected movie: " + movies.get(i).getName());
+                answer = movies.get(i).ratio();
+            }
+        } 
+        return answer;
     }
 
-    /** 
-     * Gets the number of vertices in the graph
-     * 
-     * @return the number of vertices in this graph. 
-     */
-    public int getNumVertices(){
-        return adj.getNumVertices();
-    }
-
-    /** 
-     * Checks if there is an edge between two vertices
-     * 
-     * @param  vertex1  the name of the first vertex to check
-     * @param  vertex2  the name of the second vertex to check
-     * 
-     * @return  true if an edge exists between two given vertices
-     *          which means that two corresponding arcs exist in the graph 
-     *          and false otherwise
-     */
-    public boolean isEdge (String vertex1, String vertex2){
-        return adj.isEdge(vertex1, vertex2);
-    }
-
-    /** 
-     * Adds a vertex to this graph, associating object with vertex.
-     * If the vertex already exists, nothing is inserted.
-     * 
-     * @param  vertex  the name of the vertex to be added
-     */
-    public void addVertex (String vertex){
-        adj.addVertex(vertex);
-    }
-
-    /** 
-     * Removes a single vertex with the given value from this graph.
-     * If the vertex does not exist, it does not change the graph.
-     * 
-     * @param  vertex  the name of the vertex to be removed
-     */
-    public void removeVertex (String vertex){
-        adj.removeVertex(vertex);
-    }
-
-    /** 
-     * Inserts an edge between two vertices of this graph, if the vertices 
-     * exist. Else does not change the graph. 
-     * 
-     * @param  vertex1  the name of the first vertex to add an edge to
-     * @param  vertex2  the name of the second vertex to add an edge to
-     */
-    public void addEdge (String vertex1, String vertex2){
-        adj.addEdge(vertex1, vertex2);
-    }
-
-    /** 
-     * Removes an edge between two vertices of this graph, if the vertices 
-     * exist, else does not change the graph.
-     * 
-     * @param  vertex1  the name of the first vertex to remove an edge from
-     * @param  vertex2  the name of the second vertex to remove an edge from
-     */
-    public void removeEdge (String vertex1, String vertex2){
-        adj.removeEdge(vertex1, vertex2);
-    }
-
-    /** 
-     * Saves the current graph into a .tgf file. If it cannot write the file, 
-     * a message is printed. 
-     * 
-     * @param  tgf_file_name  the name of the tgf file that will be created
-     */
-    public void saveTGF(String tgf_file_name){
-        adj.saveTGF(tgf_file_name);
-    }
-    
-    /**
-     * Creates a string version of the graph
-     * 
-     * @return  the String version of the graph
-     */
     public String toString(){
         return adj.toString();
     }
 
-    /**
-     * main runner for class HollywoodGraph
-     */
+    /** Returns true if this graph is empty, false otherwise. */
+    public boolean isEmpty(){
+        return adj.isEmpty();
+    }
+
+    /** Returns the number of vertices in this graph. */
+    public int getNumVertices(){
+        return adj.getNumVertices();
+    }
+
+    /** Returns the number of arcs in this graph. */
+    public int getNumArcs(){
+        System.out.println("There are no arcs in this graph, sorry!");
+        return -1;
+    }
+
+    /** Returns true iff a directed edge exists b/w given vertices */
+    public boolean isArc (String vertex1, String vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+        return false;
+    }
+
+    /** Returns true iff an edge exists between two given vertices
+     * which means that two corresponding arcs exist in the graph */
+    public boolean isEdge (String vertex1, String vertex2){
+        return adj.isEdge(vertex1, vertex2);
+    }
+
+    /** Adds a vertex to this graph, associating object with vertex.
+     * If the vertex already exists, nothing is inserted. */
+    public void addVertex (String vertex){
+        adj.addVertex(vertex);
+    }
+
+    /** Removes a single vertex with the given value from this graph.
+     * If the vertex does not exist, it does not change the graph. */
+    public void removeVertex (String vertex){
+        adj.removeVertex(vertex);
+    }
+
+    /** Inserts an arc between two vertices of this graph,
+     * if the vertices exist. Else it does not change the graph. */
+    public void addArc (String vertex1, String vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+    }
+
+    /** Removes an arc between two vertices of this graph,
+     * if the vertices exist. Else it does not change the graph. */
+    public void removeArc (String vertex1, String vertex2){
+        System.out.println("There are no arcs in this graph, sorry!");
+    }
+
+    /** Inserts an edge between two vertices of this graph,
+     * if the vertices exist. Else does not change the graph. */
+    public void addEdge (String vertex1, String vertex2){
+        adj.addEdge(vertex1, vertex2);
+    }
+
+    /** Removes an edge between two vertices of this graph,
+    if the vertices exist, else does not change the graph. */
+    public void removeEdge (String vertex1, String vertex2){
+        adj.removeEdge(vertex1, vertex2);
+    }
+
+    /** Saves the current graph into a .tgf file.
+    If it cannot write the file, a message is printed. */
+    public void saveTGF(String tgf_file_name){
+        adj.saveTGF(tgf_file_name);
+    }
+
     public static void main(String[] args){
         HollywoodGraph<String> hollywood = new HollywoodGraph<String>();
 
-        hollywood.graphBuilder("nextBechdel_castGender.txt");
-        //hollywood.graphBuilder("small_castGender.txt");
-        System.out.println(hollywood.toString());
-        hollywood.saveTGF("HollywoodGraph.tgf");
+        //hollywood.graphBuilder("nextBechdel_castGender.txt");
+        hollywood.graphBuilder("small_castGender.txt");
+        //System.out.println(hollywood.toString());
+        System.out.println(hollywood.genderAnalysis("Alpha")); //false
+        
+        
+
+        //hollywood.saveTGF("HollywoodGraph.tgf");
     }
-
 }
-
