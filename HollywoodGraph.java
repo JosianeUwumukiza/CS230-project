97285d11f9d9fe1098f784973d1669e9e46dca27
JosianeUwumukiza <139395160+JosianeUwumukiza@users.noreply.github.com>
2023-12-13 14:56:24 -0500

    Update HollywoodGraph.java

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -48,7 +48,6 @@ public class HollywoodGraph<T> {
 /**
 *graphBuilder reads a text file and creates an adjListsGraph object.
 *@param fName - the text file with movies and their actors
-*@return s - the undirected graph of relationships between movies and actors
 */
     public void graphBuilder(String fName){
         try{
@@ -128,7 +127,11 @@ public class HollywoodGraph<T> {
             isVisited[i] = false;
         }
     }
-
+    /**
+    *The method Analyses movies based on the percentage of women and men cast in movies
+    *@return s-the string that shows if the movie has women cast over 48%
+    *
+    */
     public String totalGenderAnalysis(){
         String s = "";
         for (int i = 0; i< movies.size(); i++){
@@ -137,11 +140,14 @@ public class HollywoodGraph<T> {
         }
         return s;
     }
-
+    /**
+    *The method calculates the ratio of female and male actors in a movie
+    *@param movieName - the movie to be analysed
+    *@return answer - a boolean true if the female actors are more that 48% and false otherwise
+    */
     public boolean genderAnalysis(String movieName){
         //int index;
         //index = movies.indexOf(movieName);
-        //return movies.get(index).ratio();
         boolean answer = false;
         for (int i = 0; i < movies.size(); i++){
             //System.out.println(movies.get(i).getName());
@@ -151,7 +157,11 @@ public class HollywoodGraph<T> {
         } 
         return answer;
     }
-
+    /**
+    *The methods gets a list of movies an actor is in.
+    *@param a- an actor whose movies we are finding
+    *@return s- the string movies actor a is casted in.
+    */
     public String getMovies(String a){
         String s = "";
         for (int i = 0; i < actors.size(); i++){
@@ -163,75 +173,115 @@ public class HollywoodGraph<T> {
         } 
         return s;
     }
-
+    /**
+    *The method gets a list of actors a movie has
+    *@param m- the name of the movie
+    *
+    */
     public void getActors(String m) {
         for (int i = 0; i < movies.size(); i++){
             //System.out.println(movies.get(i).getName());
             if (movies.get(i).getMovieName().equals(m)){
-                //System.out.println("selected movie: " + movies.get(i).getName());
-                //return movies.get(i).toString();
+                System.out.println("selected movie: " + movies.get(i).getName());
+                
             }
         } 
     }
-
+    /**
+    *checks if string a1 and a2 are both vertices in our graph
+    *@return a boolean -true if they are vertices and false otherwise.
+    */
     public boolean validNames(String a1, String a2) {
         return (names.contains(a1) && names.contains(a2));
     }
 
+    /**
+    *the method return the index of a movie or an actor in the list of names of all vertices
+    *@return- the index of a1
+    */
     public int getIndex1(String a1) {
         return names.indexOf(a1);
     }
-
+    /**
+    *writes the graph into a String
+    *@return - the string represantation of the graph
+    */
     public String toString(){
         return adj.toString();
     }
 
-    /** Returns true if this graph is empty, false otherwise. */
+    /** 
+    *Checks if the graph is empty
+    *@return - Returns true if this graph is empty, false otherwise. 
+    */
     public boolean isEmpty(){
         return adj.isEmpty();
     }
 
-    /** Returns the number of vertices in this graph. */
+    /** 
+    *Gets the number of vertices in the Hollywood Graph
+    *@return - Returns the number of vertices in this graph. 
+    */
     public int getNumVertices(){
         return adj.getNumVertices();
     }
 
-    /** Returns true iff an edge exists between two given vertices
-     * which means that two corresponding arcs exist in the graph */
+    /** 
+     *checks if the edge exist between an actor and a movie
+     *@retur - Returns true iff an edge exists between two given vertices
+     * which means that two corresponding arcs exist in the graph 
+     *@param vertex1- movie
+     *@param vertex2- actor
+     */
     public boolean isEdge (String vertex1, String vertex2){
         return adj.isEdge(vertex1, vertex2);
     }
 
     /** Adds a vertex to this graph, associating object with vertex.
-     * If the vertex already exists, nothing is inserted. */
+     * If the vertex already exists, nothing is inserted.
+     *@param vertex- a movie or actor to be added
+     */
     public void addVertex (String vertex){
         adj.addVertex(vertex);
     }
 
     /** Removes a single vertex with the given value from this graph.
-     * If the vertex does not exist, it does not change the graph. */
+     * If the vertex does not exist, it does not change the graph. 
+     *@param vertex- movie or actor to be removed
+     */
     public void removeVertex (String vertex){
         adj.removeVertex(vertex);
     }
 
     /** Inserts an edge between two vertices of this graph,
-     * if the vertices exist. Else does not change the graph. */
+     * if the vertices exist. Else does not change the graph.
+     *@param vertex1- movie
+     *@param vertex2- actor
+     */
     public void addEdge (String vertex1, String vertex2){
         adj.addEdge(vertex1, vertex2);
     }
 
     /** Removes an edge between two vertices of this graph,
-    if the vertices exist, else does not change the graph. */
+    *if the vertices exist, else does not change the graph. 
+    *@param vertex1- movie
+    *@param vertex2- actor
+    */
     public void removeEdge (String vertex1, String vertex2){
         adj.removeEdge(vertex1, vertex2);
     }
 
     /** Saves the current graph into a .tgf file.
-    If it cannot write the file, a message is printed. */
+    * If it cannot write the file, a message is printed.
+    */
     public void saveTGF(String tgf_file_name){
         adj.saveTGF(tgf_file_name);
     }
-
+    /**
+    *Gets the movie object given the name of a movie
+    *@param movie- the name of the movie
+    *@return -the movie object
+    */
     public Movie getMovieObj(String movie){
         for (int i = 0; i < movies.size(); i++){
             if (movies.get(i).getMovieName().equals(movie)){
@@ -240,7 +290,11 @@ public class HollywoodGraph<T> {
         } 
         return null;
     }
-
+     /**
+    *Gets the actor object given the name of a actor
+    *@param actor- the name of the actor
+    *@return -the actor object
+    */
     public Actor getActorObj(String actor){
         for (int i = 0; i < actors.size(); i++){
             if (actors.get(i).getActorName().equals(actor)){
@@ -249,7 +303,11 @@ public class HollywoodGraph<T> {
         } 
         return null;
     }
-
+    /**
+    *Gets the index of an actor in a list of actors
+    *@param- actorName- the actor
+    *@return i- the index of actorName
+    */
     public int getIndex(String actorName) {
         for (int i = 0; i < actors.size(); i++) {
             if (actors.get(i).getActorName().equals(actorName)) {
@@ -258,6 +316,14 @@ public class HollywoodGraph<T> {
         }
         return -1; // Return -1 if the actor is not found
     }
+    /**
+    *Returns the degree of separation between two actors  Given two actors a1 and a2, what is the number of movies that separate these actors? 
+    *For example, if the actors have played in a movie together, their separation number would be 0. If the actors have not played in a movie together, 
+    *but a1 played in a movie m1 with another actor a, who played in a movie m2 with a2, then this number would be 1. 
+    *@param a- the first actor
+    *@param b- the second actor
+    *@return int-degree of separation
+    */
 
     public int getRel1(String a, String b) {
         if (a.equals(b)) {
